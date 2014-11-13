66ee6e28836f18bce176f111d69ca488007c80d2
Luiz Paulo de Farias Jr <luiz@luizpaulo.me>
2014-11-13 18:36:39 -0200

    Fixing workers

--- app/workers/abc_curve_worker.rb
+++ app/workers/abc_curve_worker.rb
@@ -5,8 +5,8 @@ class AbcCurveWorker
     store = Store.find_by(id: store_id)
     start_dt = Date.parse(start_date)
     end_dt = Date.parse(end_date)
-    reports = abc_curve_report_for(start_dt, end_dt, email)
-    drive_url = VndaAPI::Drive.create_abc_curve_report_spreedsheet(self, reports, start_dt, end_dt)
+    reports = store.abc_curve_report_for(start_dt, end_dt, email)
+    drive_url = VndaAPI::Drive.create_abc_curve_report_spreedsheet(store, reports, start_dt, end_dt)
     store.abc_curve_reports.create(start: start_dt, end: end_dt, drive_url: drive_url)
   end
 end
--- app/workers/state_worker.rb
+++ app/workers/state_worker.rb
@@ -5,8 +5,8 @@ class StateWorker
     store = Store.find_by(id: store_id)
     start_dt = Date.parse(start_date)
     end_dt = Date.parse(end_date)
-    reports = state_report_for(start_dt, end_dt)
-    drive_url = VndaAPI::Drive.create_state_report_spreedsheet(self, reports, start_dt, end_dt, email)
+    reports = store.state_report_for(start_dt, end_dt)
+    drive_url = VndaAPI::Drive.create_state_report_spreedsheet(store, reports, start_dt, end_dt, email)
     store.state_reports.create(start: start_dt, end: end_dt, drive_url: drive_url)      
   end
 end
