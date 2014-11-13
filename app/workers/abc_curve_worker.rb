0c52a786bc879099597e090f072f0e35c46c24f4
Luiz Paulo de Farias Jr <luiz@luizpaulo.me>
2014-11-13 19:27:22 -0200

    Fixing AbcCurve

--- app/workers/abc_curve_worker.rb
+++ app/workers/abc_curve_worker.rb
@@ -5,7 +5,7 @@ class AbcCurveWorker
     store = Store.find_by(id: store_id)
     start_dt = Date.parse(start_date)
     end_dt = Date.parse(end_date)
-    reports = store.abc_curve_report_for(start_dt, end_dt, email)
+    reports = store.abc_curve_report_for(start_dt, end_dt)
     drive_url = VndaAPI::Drive.create_abc_curve_report_spreedsheet(store, reports, start_dt, end_dt, email)
     store.abc_curve_reports.create(start: start_dt, end: end_dt, drive_url: drive_url)
   end
