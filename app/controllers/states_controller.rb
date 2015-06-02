2b49ecf26457b4070bfae6ebed94d78a5478dea1
Luiz Paulo de Farias Jr <luiz@luizpaulo.me>
2015-06-02 13:25:10 -0300

    Adding states controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,16 +3,16 @@ class StatesController < ApplicationController
 
   def daily
     @reports = store.state_reports.where(date_type: "daily").order(start: :desc)
-    @first_report = @reports.first
+    @report = @reports.first
   end
 
   def weekly
     @reports = store.state_reports.where(date_type: "weekly").order(start: :desc)
-    @first_report = @reports.first
+    @report = @reports.first
   end
 
   def monthly
     @reports = store.state_reports.where(date_type: "monthly").order(start: :desc)
-    @first_report = @reports.first
+    @report = @reports.first
   end
 end
